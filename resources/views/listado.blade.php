959e2ede8c1ca1847830b09c9d52f7c37436eb95
MarvinTrey5 <santosmarvin201@gmail.com>
2024-04-21 18:10:11 -0600

    Se ordenaron los elementos del listado

--- resources/views/listado.blade.php
+++ resources/views/listado.blade.php
@@ -5,87 +5,94 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Biblioteca Virtual - Listado de Libros</title>
   <link rel="stylesheet" href="{{asset('styles/estilosTitu.css')}}">
+  <link rel="icon" href="{{asset('icon/IconB.png')}}">
 </head>
 <body>
-  <h1>Listado de Libros</h1>
-    <h1>Biblioteca Virtual</h1>
-    <br>
-    <a href="{{ url ('/') }}" target="_blank"> Página Principal</a>
-<p>
-        Titulo: "Frieren: Ascending to the Skies", <br>
-        Autor: "Yoshitaka Amano", <br>
-        Numero: 1, <br>
-        Disponible: Sí <br>
-        <img src="https://images.cdn3.buscalibre.com/fit-in/360x360/e4/1d/e41d16f1a9d0822c4baaa864c1f8a22d.jpg"> <br>
-        <a href="{{ url ('/reserva') }}" target="_blank">Reservar ahora</a> <br>
+        <header>
+                <h1>Listado de Libros</h1>
+                <h1>Biblioteca Virtual</h1>               
+        </header>
+        <nav>
+                <ul>
+                        <a href="{{ url ('/') }}" target="_self"> Página Principal</a>
+                </ul>
+        </nav>
+<div>
+        <p>Titulo: "Frieren: Ascending to the Skies" </p>
+        <p>Autor: "Yoshitaka Amano"</p>
+        <p>Número: 1</p>
+        <p>Disponible: Sí.</p>
+        <img src="https://images.cdn3.buscalibre.com/fit-in/360x360/e4/1d/e41d16f1a9d0822c4baaa864c1f8a22d.jpg"> 
+        <p><a href="{{ url ('/reserva') }}" target="_selft">Reservar ahora</a></p> 
 <hr>
-        Titulo: "Jujutsu Kaisen", <br>
-        Autor: "Gege Akutami", <br>
-        Volumen: 11, <br>
-        disponible: Sí <br>
-        <img src=https://www.normaeditorial.com/upload/media/albumes/0001/19/f2962f988ff422ff0f0e52c0cce7fe061ae4fbe6.jpeg> <br>
-        <a href="{{ url ('/reserva') }}" target="_blank">Reservar ahora</a> <br>
+        <p>Titulo: "Jujutsu Kaisen"</p> 
+        <p>Autor: "Gege Akutami"</p>
+        <p>Volumen: 11</p>
+        <p>Disponible: Sí</p>
+        <img src="https://www.normaeditorial.com/upload/media/albumes/0001/19/f2962f988ff422ff0f0e52c0cce7fe061ae4fbe6.jpeg"> 
+        <p><a href="{{ url ('/reserva') }}" target="_self">Reservar ahora</a></p> 
 <hr>
-        Titulo: "Kimetsu no Yaiba", <br>
-        Autor: "Koyoharu Gotouge", <br>
-        Numero: 20, <br>
-        Disponible: Sí <br>
-        <img src=https://m.media-amazon.com/images/I/81bfP2ZLKqL._AC_UF1000,1000_QL80_.jpg> <br>
-        <a href="{{ url ('/reserva') }}" target="_blank">Reservar ahora</a> <br>
+        <p>Titulo: "Kimetsu no Yaiba"</p>
+        <p>Autor: "Koyoharu Gotouge"</p>
+        <p>Número: 20 </p>
+        <p>Disponible: Sí </p>
+        <img src="https://m.media-amazon.com/images/I/81bfP2ZLKqL._AC_UF1000,1000_QL80_.jpg"> 
+        <p><a href="{{ url ('/reserva') }}" target="_self">Reservar ahora</a></p>
 <hr>
-        Titulo: "Fate Zero", <br>
-        Autor: "Gen Urobuchi", <br>
-        Numero: 5, <br>
-        Disponible: Sí <br>
-        <img src=https://m.media-amazon.com/images/I/91vMRinz5vL._SL1500_.jpg> <br>
-        <a href="{{ url ('/reserva') }}" target="_blank">Reservar ahora</a> <br>
+        <p>Titulo: "Fate Zero" </p>
+        <p>Autor: "Gen Urobuchi" </p>
+        <p>Número: 5</p>
+        <p>Disponible: Sí </p>
+        <img src="https://m.media-amazon.com/images/I/91vMRinz5vL._SL1500_.jpg"> 
+        <p><a href="{{ url ('/reserva') }}" target="_self">Reservar ahora</a></p> 
 <hr>
-        Titulo: "hokkaido gals are super adorable", <br>
-        Autor: "Kai Ikada", <br>
-        Numero: 1, <br>
-        Disponible: Sí <br>
-        <img src=https://upload.wikimedia.org/wikipedia/en/7/78/Hokkaido_Gals_Vol_1.jpg> <br>
-        <a href="{{ url ('/reserva') }}" target="_blank">Reservar ahora</a> <br>
+        <p>Titulo: "hokkaido gals are super adorable"</p> 
+        <p>Autor: "Kai Ikada"</p>
+        <p>>Número: 1</p>
+        <p>Disponible: Sí</p> 
+        <img src="https://upload.wikimedia.org/wikipedia/en/7/78/Hokkaido_Gals_Vol_1.jpg"> 
+        <p><a href="{{ url ('/reserva') }}" target="_self">Reservar ahora</a></p> 
 <hr>  
-        Titulo: "FullMetal Alchemist Brothehood, <br>
-        Autor: "Hiromu Arakawa", <br>
-        Numero: 6, <br>
-        Disponible: Sí <br>
-        <img src=https://m.media-amazon.com/images/I/71xOnCxDTgL._AC_UF1000,1000_QL80_.jpg> <br>
-        <a href="{{ url ('/reserva') }}" target="_blank">Reservar ahora</a> <br>
+        <p>Titulo: "FullMetal Alchemist Brothehood"</p> 
+        <p>Autor: "Hiromu Arakawa"</p> 
+        <p>Número: 6</p>
+        <p>Disponible: Sí </p>
+        <img src="https://m.media-amazon.com/images/I/71xOnCxDTgL._AC_UF1000,1000_QL80_.jpg"> 
+        <p><a href="{{ url ('/reserva') }}" target="_self">Reservar ahora</a></p> 
 <hr>
-        Titulo: "Boku no Kokoro no Yabai Yatsu", <br>
-        Autor: "Norio Sakurai", <br>
-        Numero: 3, <br>
-        Disponible: Sí <br>
-        <img src=https://www.manga-news.com/public/images/vols/Boku-no-Kokoro-no-Yabai-Yatsu-3-jp.jpg> <br>
-        <a href="{{ url ('/reserva') }}" target="_blank">Reservar ahora</a> <br>
+        <p>Titulo: "Boku no Kokoro no Yabai Yatsu"</p> 
+        <p>Autor: "Norio Sakurai"</p>
+        <p>Número: 3</p>
+        <p>Disponible: Sí </p>
+        <img src="https://www.manga-news.com/public/images/vols/Boku-no-Kokoro-no-Yabai-Yatsu-3-jp.jpg"> 
+        <p><a href="{{ url ('/reserva') }}" target="_self">Reservar ahora</a> </p>
 <hr>
-        Titulo: "Naruto", <br>
-        Autor: "Masashi Kishimoto", <br>
-        Numero: 40, <br>
-        Disponible: Sí <br>
-        <img src=https://d3tvwjfge35btc.cloudfront.net/Assets/29/007/L_p1005100729.jpg> <br>
-        <a href="{{ url ('/reserva') }}" target="_blank">Reservar ahora</a> <br>
+        <p>Titulo: "Naruto" </p>
+        <p>Autor: "Masashi Kishimoto"</p>
+        <p>Número: 40</p>
+        <p>Disponible: Sí</p> 
+        <img src="https://d3tvwjfge35btc.cloudfront.net/Assets/29/007/L_p1005100729.jpg"> 
+        <p><a href="{{ url ('/reserva') }}" target="_self">Reservar ahora</a></p> 
 <hr>
-        Titulo: "One Piece", <br>
-        Autor: "Eiichiro Oda", <br>
-        Numero: 100, <br>
-        Disponible: Sí <br>
-        <img src=https://m.media-amazon.com/images/I/A1vTNRncVyL._AC_UF1000,1000_QL80_.jpg> <br>
-        <a href="{{ url ('/reserva') }}" target="_blank">Reservar ahora</a> <br>
+        <p>Titulo: "One Piece"</p> 
+        <p>Autor: "Eiichiro Oda" </p>
+        <p>Número: 100</p>
+        <p>Disponible: Sí</p> 
+        <img src="https://m.media-amazon.com/images/I/A1vTNRncVyL._AC_UF1000,1000_QL80_.jpg"> 
+        <p><a href="{{ url ('/reserva') }}" target="_self">Reservar ahora</a></p> 
 <hr>
-        Titulo: "The quintessential quintuplets", <br>
-        Autor: "Negi Haruba", <br>
-        Numero: 14, <br>
-        Disponible: Sí <br>
-        <img src=https://www.normaeditorial.com/upload/media/albumes/0001/26/eed0314e0e2606d529e81f70a248a5835a295a8b.jpeg> <br>
-        <a href="{{ url ('/reserva') }}" target="_blank">Reservar ahora</a> <br>
-
-    </p>
-  <section class="libros">
-    </section>
-
-  <script src="listado.js"></script>
+        <p>Titulo: "The quintessential quintuplets"</p> 
+        <p>Autor: "Negi Haruba"</p>
+        <p>Número: 14</p>
+        <p>Disponible: Sí</p>
+        <img src="https://www.normaeditorial.com/upload/media/albumes/0001/26/eed0314e0e2606d529e81f70a248a5835a295a8b.jpeg"> 
+        <p><a href="{{ url ('/reserva') }}" target="_self">Reservar ahora</a></p> 
+</div>
+  <footer>
+   <span>
+    UNIVERSIDAD GERARDO BARRIOS
+    PRESENTACIÓN PARCIAL COMPUTO II
+   </span>
+  </footer>
 </body>
 </html>
