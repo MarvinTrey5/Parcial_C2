17a5da598e03e5e6c1e16df8a209420b7df8f7f7
MarvinTrey5 <santosmarvin201@gmail.com>
2024-04-21 18:11:08 -0600

    Modificación de la vista en la reserva

--- resources/views/reserva.blade.php
+++ resources/views/reserva.blade.php
@@ -5,10 +5,26 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Reserva exitosa</title>  
   <link rel="stylesheet" href="{{asset('styles/estilosReser.css')}}"> 
+  <link rel="icon" href="{{asset('icon/IconB.png')}}">
 </head>
 <body>
+<header>
+    <img src="https://www.pngitem.com/pimgs/m/9-94185_libro-los-libros-libros-de-la-biblioteca-la.png" alt="Imagen de biblioteca">
+    <h1>Biblioteca Virtual</h1>
+    <h2>Un universo de conocimiento a tu alcance</h2>
+  </header>
+  <nav>
+        <ul>
+            <li><a href="{{ url ('/listado') }}" target="_self" class="btn btn-explorar">Explora nuestra colección</a></li>
+        </ul>
+    </nav>
   <h1>Reserva exitosa</h1>
   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp2IDwTxzYI_GTjHB7pMuWDnhWKWv762W_yw&usqp=CAU alt="Reserva exitosa"> <p>¡Su libro ha sido reservado!</p>
-  <a href="{{ url ('/listado') }}" target="_blank">Volver a la página de libros</a>
+  <footer>
+   <span>
+    UNIVERSIDAD GERARDO BARRIOS
+    PRESENTACIÓN PARCIAL COMPUTO II
+   </span>
+  </footer>
 </body>
 </html>
