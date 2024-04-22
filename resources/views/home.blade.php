0565203c79e6ef3fe8bbb84baea256e418c630f7
MarvinTrey5 <santosmarvin201@gmail.com>
2024-04-21 18:09:33 -0600

    Se agregaron datos para ordenar la vista general

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,35 +5,112 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Principal</title>
     <link rel="stylesheet" href="{{asset('styles/estilosPrin.css')}}">
+    <link rel="icon" href="{{asset('icon/IconB.png')}}">
 </head>
 <body>
 <header>
-    <img src="https://w1.pngwing.com/pngs/968/193/png-transparent-india-design-library-digital-library-academic-library-digital-library-of-india-education-learning-ebook-thumbnail.png" alt="Imagen de biblioteca">
+    <img src="https://www.pngitem.com/pimgs/m/9-94185_libro-los-libros-libros-de-la-biblioteca-la.png" alt="Imagen de biblioteca">
     <h1>Biblioteca Virtual</h1>
-    <p>Un universo de conocimiento a tu alcance</p>
+    <h2>Un universo de conocimiento a tu alcance</h2>
   </header>
+  <nav>
+        <ul>
+            <li><a href="{{ url ('/listado') }}" target="_self" class="btn btn-explorar">Explora nuestra colección</a></li>
+        </ul>
+    </nav>
+    <section>
+        <article>
+            <p id="parrafo">
+            ¡Bienvenido a nuestra biblioteca virtual, donde el conocimiento y la imaginación convergen en un universo digital sin límites! Aquí, 
+            cada clic es una invitación a explorar un vasto océano de libros electrónicos, revistas académicas y recursos educativos de todas las 
+            disciplinas imaginables. Ya sea que busques sumergirte en las páginas de la literatura clásica, descubrir las últimas investigaciones 
+            científicas o explorar nuevos horizontes en el arte y la cultura, nuestra biblioteca virtual está lista para acompañarte en tu viaje 
+            intelectual. Con la comodidad de acceso desde cualquier lugar y en cualquier momento, te invitamos a embarcarte en una experiencia de
+            aprendizaje sin fronteras. ¡Bienvenido a un mundo donde el conocimiento es infinito y la curiosidad es bienvenida!.
 
-  <main>
-    <section class="caracteristicas">
-      <h2>Características principales</h2>
-      <ul>
-        <li>Préstamo de libros en línea</li>
-        <li>Amplia colección de materiales</li>
-        <li>Disponibilidad en tiempo real</li>
-        <li>Listas de lectura personalizadas</li>
-        <li>Acceso a recursos en línea</li>
-      </ul>
-    </section>
-
-    <section class="llamada-accion">
-        <h2>¡Empieza a disfrutar hoy mismo!</h2>
-        <a href="{{ url ('/listado') }}" target="_blank" class="btn btn-explorar">Explora nuestra colección</a>
-      </section>
-  </main>
-
+            En nuestra biblioteca virtual, cada sección es un mundo por descubrir. Desde las estanterías digitales repletas de clásicos de la literatura mundial 
+            hasta las bases de datos abarrotadas de investigaciones científicas de vanguardia, aquí encontrarás un vasto tesoro de conocimiento que espera ser 
+            explorado. Sumérgete en las profundidades de la historia antigua, donde las civilizaciones perdidas cobran vida a través de relatos fascinantes, o 
+            aventúrate en los confines del universo conocido y desconocido con los últimos avances en astronomía y cosmología. Si el arte y la creatividad son 
+            tu pasión, déjate inspirar por galerías virtuales que albergan obras maestras de todos los estilos y épocas, o explora las colecciones de música y 
+            cine que abarcan géneros y culturas de todo el mundo. Además, nuestra biblioteca no se limita a las palabras escritas; también ofrece una amplia gama 
+            de recursos audiovisuales, interactivos y multimedia que enriquecerán tu experiencia de aprendizaje de maneras nuevas y emocionantes. Con acceso 
+            instantáneo desde cualquier dispositivo conectado a internet, esta biblioteca virtual se convierte en tu compañero constante en el viaje hacia el 
+            conocimiento, la comprensión y la inspiración.
+            </p>          
+            <div class="datos">
+              <label>
+                Los tomos de anime son como vastos océanos de creatividad y emoción, donde cada episodio es una ola que nos sumerge en un mundo lleno de aventura, 
+                intriga y pasión. Desde las cumbres de las montañas más altas hasta las profundidades del espacio infinito, cada tomo nos lleva en un viaje inolvidable 
+                a través de paisajes exuberantes y escenarios impresionantes. Los personajes cobran vida con cada trazo de animación, revelando capas de complejidad y 
+                profundidad que nos cautivan y nos hacen conectar con sus luchas y triunfos. Cada tomo es un lienzo en movimiento, donde se entrelazan la narrativa, la 
+                música y la animación para crear una experiencia sensorial que nos transporta a mundos más allá de nuestra imaginación.
+                La producción de un tomo de anime implica un equipo multidisciplinario que incluye guionistas, directores, animadores, diseñadores de personajes, 
+                compositores de música y otros profesionales creativos. Juntos, trabajan para dar vida a los mundos y personajes, creando una experiencia inmersiva para 
+                el espectador.Cada tomo de anime puede tener una duración variable, desde unos pocos episodios hasta decenas, dependiendo de la complejidad de la historia 
+                y de las decisiones de producción. Algunas series adaptan material de manga o novelas ligeras, mientras que otras presentan historias originales creadas 
+                específicamente para la animación.La popularidad del anime ha crecido significativamente a nivel mundial en las últimas décadas, lo que ha llevado a la 
+                producción de una amplia gama de tomos que abordan temas diversos y atraen a una audiencia global. Los tomos de anime no solo entretienen, sino que también
+                pueden transmitir mensajes importantes, explorar temas profundos y ofrecer una ventana a la cultura japonesa y a la creatividad de sus creadores.
+              </label>
+            </div>
+        </article>
+            <main class="color">
+              <h1>TOPS DE TOMOS DE NUESTRA BIBLIOTECA</h1>
+              <div class="galeria">
+                <article class="imagen1">
+                  <figure>
+                    <img 
+                      src="https://www.figurasmranime.cl/wp-content/uploads/2022/12/jujutsu-kaisen-tomo004.png"> 
+                      <h3>Jujutsu Kaisen</h3>
+                  </figure>
+                </article>
+                <article class="imagen1">
+                  <figure>
+                    <img 
+                      src="https://images.cdn3.buscalibre.com/fit-in/360x360/e4/1d/e41d16f1a9d0822c4baaa864c1f8a22d.jpg"> 
+                      <h3>Frieren: Ascending to the Skies</h3>
+                  </figure>
+                </article>
+                <article class="imagen1">
+                  <figure>
+                    <img 
+                      src="https://m.media-amazon.com/images/I/81bfP2ZLKqL._AC_UF1000,1000_QL80_.jpg"> 
+                      <h3>Kimetsu No Yaiba</h3>
+                  </figure>
+                </article>
+              </div>
+            </main>
+        <main class="color">
+              <div class="galeria">
+                <article class="imagen1">
+                  <figure>
+                    <img 
+                      src="https://m.media-amazon.com/images/I/91vMRinz5vL._SL1500_.jpg"> 
+                      <h3>Fate Zero</h3>
+                  </figure>
+                </article>
+                <article class="imagen1">
+                  <figure>
+                    <img 
+                      src="https://m.media-amazon.com/images/I/71xOnCxDTgL._AC_UF1000,1000_QL80_.jpg"> 
+                      <h3>FullMetal Alchemist Brothehood</h3>
+                  </figure>
+                </article>
+                <article class="imagen1">
+                  <figure>
+                    <img 
+                      src="https://m.media-amazon.com/images/I/A1vTNRncVyL._AC_UF1000,1000_QL80_.jpg"> 
+                      <h3>One Piece</h3>
+                  </figure>
+                </article>
+              </div>
+          </main>
   <footer>
-    <p>&copy; Parcial II</p>
-    <a href="#" class="contacto">Contáctanos</a>
+   <span>
+    UNIVERSIDAD GERARDO BARRIOS
+    PRESENTACIÓN PARCIAL COMPUTO II
+   </span>
   </footer>
 </body>
 </html>
